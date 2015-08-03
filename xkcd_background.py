bfed444946cc4c36a3ae1b6cd1d69c453dcd13bd
peter mitrano <mitranopeter@gmail.com>
2015-08-02 21:18:55 -0400

    tested script. works from command line.

--- xkcd_background.py
+++ xkcd_background.py
@@ -1,10 +1,12 @@
-import os
+import subprocess
 import json
 import urllib2
 
 json_file = urllib2.urlopen('http://xkcd.com/info.0.json')
 file_contents = json_file.read()
 as_json = json.loads(file_contents)
-img_uri = as_json['img']
-cmd = "gsettings gnome.org.desktop.background picture-uri " + img_uri
+img_url = str(as_json['img'])
+download = ["wget", img_url, "-O", "/home/peter/.wallpaper.png"]
+subprocess.call(download)
+cmd = ["gsettings","set" , "org.gnome.desktop.background",  "picture-uri", "file:///home/peter/.wallpaper.png"]
 subprocess.call(cmd)
